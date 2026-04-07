d0b179e787f26a04d2a1cd6606408a883ff8a45f
Rodrigo Piccilli <145203522+RodrigoPiccilli@users.noreply.github.com>
2026-04-07 07:45:11 -0400

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        List<Integer> current = new ArrayList<>();
+
+        for(int n : nums) {
+            current.add(n);
+            backtrack(nums, current, res);
+            current.remove(current.size() - 1);
+        }
+
+        return res;
+
+    }
+
+    private void backtrack(int[] nums, List<Integer> current, List<List<Integer>> res) {
+
+        if(current.size() == nums.length) {
+            res.add(new ArrayList<>(current));
+            return;
+        }
+
+        for(int n : nums) {
+
+            if(current.contains(n)) continue;
+            current.add(n);
+            backtrack(nums, current, res);
+            current.remove(current.size() - 1);
+
+        }
+
+    }
+}
