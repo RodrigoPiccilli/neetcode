1b6ca7880331565ae31bf3320ec33facddf15973
Rodrigo Piccilli <145203522+RodrigoPiccilli@users.noreply.github.com>
2026-04-17 06:49:44 -0400

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.java
@@ -0,0 +1,49 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+
+        if(node == null) return null;
+
+        Map<Integer, Node> clones = new HashMap<>();
+
+        dfs(node, clones);
+
+        return clones.get(node.val);
+
+        
+    }
+
+    private void dfs(Node node, Map<Integer, Node> clones) {
+
+        if(clones.containsKey(node.val)) {
+            return;
+        }
+
+        clones.put(node.val, new Node(node.val));
+
+        for(Node n : node.neighbors) {
+            dfs(n, clones);
+            clones.get(node.val).neighbors.add(clones.get(n.val));
+        }
+
+    }
+}
