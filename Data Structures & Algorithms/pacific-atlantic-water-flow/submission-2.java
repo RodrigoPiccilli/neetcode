0afcef7508c40344878e9c5caf75c5d4a0ede12e
Rodrigo Piccilli <145203522+RodrigoPiccilli@users.noreply.github.com>
2026-04-20 07:09:42 -0400

    Add: pacific-atlantic-water-flow - submission-2

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-2.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    int ROWS;
+    int COLS;
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+
+        ROWS = heights.length;
+        COLS = heights[0].length;
+        
+        List<List<Integer>> res = new ArrayList<>();
+
+        boolean[][] pacific = new boolean[ROWS][COLS];
+        boolean[][] atlantic = new boolean[ROWS][COLS];
+
+        for(int c = 0; c < COLS; c++) {
+            dfs(0, c, heights, pacific);
+            dfs(ROWS - 1, c, heights, atlantic);
+        }
+
+        for(int r = 0; r < ROWS; r++) {
+            dfs(r, 0, heights, pacific);
+            dfs(r, COLS - 1, heights, atlantic);
+        }
+
+        for(int r = 0; r < ROWS; r++) {
+            for(int c = 0; c < COLS; c++) {
+                if(pacific[r][c] && atlantic[r][c]) {
+                    res.add(Arrays.asList(r, c));
+                }
+            }
+        }
+
+        return res;
+
+
+    }
+
+    private void dfs(int r, int c, int[][] heights, boolean[][] ocean) {
+
+        ocean[r][c] = true;
+
+        if(r - 1 >= 0 && r - 1 < ROWS && heights[r - 1][c] >= heights[r][c] && !ocean[r - 1][c]) dfs(r - 1, c, heights, ocean);
+        if(r + 1 >= 0 && r + 1 < ROWS && heights[r + 1][c] >= heights[r][c] && !ocean[r + 1][c]) dfs(r + 1, c, heights, ocean);
+        if(c - 1 >= 0 && c - 1 < COLS && heights[r][c - 1] >= heights[r][c] && !ocean[r][c - 1]) dfs(r, c - 1, heights, ocean);
+        if(c + 1 >= 0 && c + 1 < COLS && heights[r][c + 1] >= heights[r][c] && !ocean[r][c + 1]) dfs(r, c + 1, heights, ocean);
+
+
+    }
+}
