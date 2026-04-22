652cbf3db334dacdd582979ea32a3a788e8d4b76
Rodrigo Piccilli <145203522+RodrigoPiccilli@users.noreply.github.com>
2026-04-22 08:02:29 -0400

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        Set<Integer> visited = new HashSet<>();
+
+        for (int[] p : prerequisites) {
+            map.putIfAbsent(p[0], new ArrayList<>());
+            map.get(p[0]).add(p[1]);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(i, map, visited)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean dfs(int course, Map<Integer, List<Integer>> map, Set<Integer> visited) {
+       
+        if(visited.contains(course)) return false;
+       
+        List<Integer> prereq = map.get(course);
+
+        visited.add(course);
+
+        if(prereq != null) {
+        for(int c : prereq) {
+
+            if(!dfs(c, map, visited)) return false;
+
+        }
+        }
+
+        visited.remove(course);
+        map.put(course, new ArrayList<>());
+
+        return true;
+    }
+}
