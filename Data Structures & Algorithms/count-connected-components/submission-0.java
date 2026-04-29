95877d20bd253ee1eb0f6c3a9b27b19fd7080e05
Rodrigo Piccilli <145203522+RodrigoPiccilli@users.noreply.github.com>
2026-04-29 08:09:56 -0400

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.java
@@ -0,0 +1,51 @@
+class Solution {
+    
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    Set<Integer> visited = new HashSet<>();
+    
+    public int countComponents(int n, int[][] edges) {
+
+        for(int i = 0; i < n; i++) {
+            map.put(i, new ArrayList<>());
+        }
+
+        for(int[] pair : edges) {
+            map.get(pair[0]).add(pair[1]);
+            map.get(pair[1]).add(pair[0]);
+        }
+
+        int count = 0;
+        int node = 0;
+
+        while(visited.size() != n) {
+
+            if(!visited.contains(node)) {
+                dfs(node, -1);
+                count++;
+            }
+            
+            node++;
+
+        }
+
+        return count;
+
+    }
+
+    private void dfs(int node, int parent) {
+
+        if(visited.contains(node)) return;
+
+        visited.add(node);
+
+        List<Integer> connected = map.get(node);
+
+        for(int c : connected) {
+
+            if(c == parent) continue;
+            dfs(c, node);
+
+        }
+
+    }
+}
