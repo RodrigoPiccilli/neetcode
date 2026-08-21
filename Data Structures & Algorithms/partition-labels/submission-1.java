ac316f176c5c1a76aaa7ecaf887825fae404b559
Rodrigo Piccilli <145203522+RodrigoPiccilli@users.noreply.github.com>
2026-08-21 17:19:23 -0400

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        
+        Map<Character, Integer> last = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+
+        for (char c : s.toCharArray()) {
+            if (!last.containsKey(c)) {
+                last.put(c, s.lastIndexOf(c));
+            }
+        }
+
+        int size = 0;
+        int maxIdx = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            maxIdx = Math.max(maxIdx, last.get(c));
+
+            size++;
+
+            if (i == maxIdx) {
+                res.add(size);
+                size = 0;
+            }
+        }
+
+        return res;
+    }
+}
