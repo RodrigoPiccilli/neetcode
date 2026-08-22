828e44a31cb15116f39c3d16bc4e307d1766a004
Rodrigo Piccilli <145203522+RodrigoPiccilli@users.noreply.github.com>
2026-08-21 20:20:35 -0400

    Add: merge-triplets-to-form-target - submission-2

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+
+        int[] cur = new int[] {-1, -1, -1};
+
+        for(int[] triplet : triplets) {
+
+            if(triplet[0] > target[0] || triplet[1] > target[1] || triplet[2] > target[2]) {
+                continue;
+            }
+
+            cur[0] = Math.max(cur[0], triplet[0]);
+            cur[1] = Math.max(cur[1], triplet[1]);
+            cur[2] = Math.max(cur[2], triplet[2]);
+
+        }
+
+        return Arrays.equals(cur, target);
+        
+    }
+}
