61ff7e026d49b679564f7642de25215a4e40736b
Rodrigo Piccilli <145203522+RodrigoPiccilli@users.noreply.github.com>
2026-08-21 21:12:58 -0400

    Add: valid-sudoku - submission-8

---
+++ Data Structures & Algorithms/valid-sudoku/submission-8.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        seen = set()
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+
+                val = board[i][j];
+
+                if val == ".":
+                    continue
+
+                row = val + "Row" + str(i)
+                col = val + "Col" + str(j)
+                quad = val + "Quadrant" +  str(i // 3) + "," + str(j // 3)
+
+                if row in seen: 
+                    return False
+                else: 
+                    seen.add(row)
+
+                if col in seen: 
+                    return False
+                else: 
+                    seen.add(col)
+
+                if quad in seen: 
+                    return False
+                else: 
+                    seen.add(quad)
+
+        return True
+                
+                
